02ed063490c6971a30db14e3715af6b2de3d8d65
Vlad Kuznetsov <ghost.vkv@gmail.com>
2024-03-26 14:36:54 +0300

    feat: connect to postgres, minio

--- packages/strapi1/config/plugins.js
+++ packages/strapi1/config/plugins.js
@@ -1,1 +1,41 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        // Когда задаем baseUrl в providerOptions, а не в
+        // providerOptions.s3Options, то возникает такое предупреждение:
+        //
+        // Loading Strapi(node:97542) Warning: S3 configuration options passed
+        // at root level of the plugin's providerOptions is deprecated and will
+        // be removed in a future release. Please wrap them inside the
+        // 's3Options:{}' property.
+        //
+        // Но если перенести baseUrl, rootUrl в s3Options, то после трансфера
+        // файлы будут не доступны. Периодически надо проверять это поведение и
+        // когда в Strapi исправят этот баг нужно перенести
+        // providerOptions.baseUrl -> providerOptions.s3Options.baseUrl Адрес
+        // доступный публично, формат: <s3-url>/<s3-bucket> Все файлы
+        // складываются в один бакет (anonymous download)
+        baseUrl: env('S3_BASE_URL', 'http://localhost:9000/strapi1'),
+        s3Options: {
+          // Тут можно подставить адрес относительно внутренней сети
+          endpoint: env('S3_ENDPOINT', 'http://localhost:9000'),
+          // Если true, то путь к бакету и объектам определяется как <s3-addr>/<bucket>/<path-to-obj>
+          forcePathStyle: env.bool('S3_FORCE_PATH_STYLE', true),
+          region: env('S3_REGION', 'central1'),
+          params: {
+            // Публичный бакет (anonymous download)
+            Bucket: env('S3_BUCKET', 'strapi1')
+          },
+          credentials: {
+            // Логин
+            accessKeyId: env('S3_ACCESS_KEY_ID', 'admin'),
+            // Пароль
+            secretAccessKey: env('S3_ACCESS_SECRET', 'admin1admin1')
+          }
+        }
+      }
+    }
+  }
+});
--- packages/strapi2/config/middlewares.js
+++ packages/strapi2/config/middlewares.js
@@ -1,12 +1,28 @@
-module.exports = [
-  'strapi::logger',
+module.exports = ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          // working MinIO previews
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', env('MINIO_ASSETS_HOST', 'localhost:9000')],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', env('MINIO_ASSETS_HOST', 'localhost:9000')],
+          // working GraphQL Playground
+          'style-src': ["'self'", 'https:', "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          upgradeInsecureRequests: null
+        }
+      }
+    }
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
-  'strapi::public',
+  'strapi::public'
 ];
